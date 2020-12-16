bb654895d698d0183d8fa3735423e62cc423e95a
Kizhakkarakkatt Dominic <ski@softwareag.com>
2020-12-16 17:13:32 +0530

    update pages

--- template/source/src/app/app-routing.module.ts
+++ template/source/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: '', redirectTo: "login", pathMatch: "full" }];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: '', redirectTo: "home", pathMatch: "full" }, { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
