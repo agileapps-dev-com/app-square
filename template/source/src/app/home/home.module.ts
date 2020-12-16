5b1db6c7fefdf006076f25b365349f89c9c56da7
Kizhakkarakkatt Dominic <ski@softwareag.com>
2020-12-16 18:24:19 +0530

    update content

--- template/source/src/app/home/home.module.ts
+++ template/source/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule
-  ]
+  ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule { }
